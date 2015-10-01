579832ab4c3daffc25b58ce90c8c2dbd8600dee1
yerin <skant121@nate.com>
2015-10-01 05:56:52 +0000

    first

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,26 @@
+class HomeController < ApplicationController
+    def index
+        
+    end
+    
+    def about
+        
+    end
+    
+    def 예린
+        
+    end
+    
+    def 지민
+        
+    end
+    
+    def 현주
+        
+    end
+    
+    def 지영
+        
+    end
+    
+end
